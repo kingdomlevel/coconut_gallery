73842453ab49c90634efbe59158e98dbed3c0997
johnpooley <58818433+johnpooley@users.noreply.github.com>
2020-06-14 14:23:56 +0100

    Update carrierwave.rb
    hidden key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIATYQVUX7CMNK2YIM2',                        # required unless using use_iam_profile
-    aws_secret_access_key: '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',                        # required unless using use_iam_profile
+    aws_access_key_id:     'xxx',                        # required unless using use_iam_profile
+    aws_secret_access_key: 'yyy',                        # required unless using use_iam_profile
 
   }
   config.fog_directory  = 'railsagain'                                      # required  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
