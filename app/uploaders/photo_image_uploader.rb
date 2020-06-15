dd63596bbf307ac72cb153dcf789cbe07887604f
kingdomlevel <nllmrrs@aol.com>
2020-06-15 17:35:05 +0100

    modifying storage path in s3 bucket to datetime instead ID for dev purposes

--- app/uploaders/photo_image_uploader.rb
+++ app/uploaders/photo_image_uploader.rb
@@ -10,7 +10,7 @@ class PhotoImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{model.created_at}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -42,9 +42,9 @@ class PhotoImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
