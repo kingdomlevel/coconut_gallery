c2bac23e41efd9e2012db24e42b3e003e2e78b4a
JohnPooley <thevinylvandal@hotmail.com>
2020-06-16 22:04:40 +0100

    camera showing on screen

--- app/javascript/packs/photo.js
+++ app/javascript/packs/photo.js
@@ -1,1 +1,16 @@
-console.log("Hello, world, from photo.js");
+// console.log("Hello, world, from photo.js");
+
+const selfieCam = document.querySelector("#selfie-cam");
+
+const constraints = {
+  video:{
+    width:120,
+    height:1280
+  }
+
+};
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(stream =>{
+  selfieCam.srcObject = stream;
+});
