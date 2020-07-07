72568f9f16401c9c81cc54c70728b3882410c578
JohnPooley <thevinylvandal@hotmail.com>
2020-07-07 16:26:06 +0100

    drawing is saved and uploaded to S3 bucket

--- app/javascript/packs/drawing.js
+++ app/javascript/packs/drawing.js
@@ -1,5 +1,8 @@
 const canvas = document.querySelector('#new-drawing');
 const refPhoto = document.querySelector('#ref-photo');
+const submitButton = document.querySelector('#submit-drawing');
+const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+
 
 canvas.width = refPhoto.width;
 canvas.height = refPhoto.height;
@@ -10,3 +13,23 @@ context.beginPath();
 context.fillStyle = "red";
 context.rect(10,20,100,200);
 context.fill();
+
+submitButton.addEventListener("click",()=>{
+  const dataURL=canvas.toDataURL();
+
+  const newDrawing={
+    flagged_innapropriate:false,
+    moderated:false,
+    photo_id:refPhoto.dataset.id,
+    picture:dataURL
+  }
+  fetch(`/photos/${refPhoto.dataset.id}/drawings`,{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+      "X-CSRF-Token":csrf
+    },
+    body:JSON.stringify(newDrawing)
+
+  })
+})
