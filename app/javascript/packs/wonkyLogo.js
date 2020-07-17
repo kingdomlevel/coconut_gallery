5380eb48edcf4d15615766f093a2cf71aea38682
kingdomlevel <nllmrrs@aol.com>
2020-07-17 11:42:18 +0100

    making buttons prettier

--- app/javascript/packs/wonkyLogo.js
+++ app/javascript/packs/wonkyLogo.js
@@ -13,6 +13,7 @@ const makeSpans = selector => {
 // make spans from title
 makeSpans('header h1');
 makeSpans('.navBar a');
+makeSpans('section#camera #controls button');
 
 document.querySelectorAll("header h1").forEach(function (element) {
     element.childNodes.forEach(function (char) {
