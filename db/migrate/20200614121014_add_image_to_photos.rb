6beb6ea694135d625ed58986521609830c3751c4
kingdomlevel <nllmrrs@aol.com>
2020-07-18 20:15:18 +0100

    removed unnecessary migrations that were preventing DB working

--- db/migrate/20200614121014_add_image_to_photos.rb
+++
@@ -1,7 +0,0 @@
-class AddImageToPhotos < ActiveRecord::Migration[6.0]
-  def change
-    # add_column :photos, :picture, :string
-  
-
-  end
-end
--- db/migrate/20200614121818_remove_column.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumn < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :photos, :image, :string
-
-  end
-end
